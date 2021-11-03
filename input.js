0b2f7fda09a632c0492ffe2805ab5ab791f3430d
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-11-03 17:53:19 -0400

    send server move commands

--- input.js
+++ input.js
@@ -1,16 +1,49 @@
-// setup interface to handle user input from stdin
-const handleUserInput = function (key) {
-  if(key === '\u0003') {
-    process.exit();
-  }
-};
-const setupInput = function () {
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
   stdin.on("data", handleUserInput);
   return stdin;
+};
+
+// setup interface to handle user input from stdin
+const handleUserInput = function (key) {
+  switch (key) {
+    case '\u0057':
+      connection.write('Move: up');
+      break;
+    case '\u0077':
+      connection.write('Move: up');
+      break;
+    case '\u0041':
+      connection.write('Move: left');
+      break;
+    case '\u0061':
+      connection.write('Move: left');
+      break;
+    case '\u0053':
+      connection.write('Move: down');
+      break;
+    case '\u0073':
+      connection.write('Move: down');
+      break;
+    case '\u0044':
+      connection.write('Move: right');
+      break;
+    case '\u0064':
+      connection.write('Move: right');
+      break;
+    case '\u0003':
+      process.exit();
+      break;
+    default:
+      break;
+  }
+
 };
 
 module.exports = {
--- play.js
+++ play.js
@@ -3,6 +3,6 @@ const {connect} = require('./client');
 const {setupInput} = require('./input')
 // establishes a connection with the game server
 console.log("Connecting ...");
-connect();
+const conn = connect();
 
-setupInput();
+setupInput(conn);
